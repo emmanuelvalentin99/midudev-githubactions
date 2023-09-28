f9209f35e71ed6abdb16de0de6eaae6eb88303a2
emmanuelvalentin99 <emmanuelvalentin99@gmail.com>
2023-09-28 17:22:00 -0600

    fix - eslint issues

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,8 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   video: false,
   e2e: {
-    setupNodeEvents(on, config) {},
+    setupNodeEvents(on, config) {
+      console.log(on,config)
+    },
   }
 })
